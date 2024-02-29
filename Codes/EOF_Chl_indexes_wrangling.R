c8c46fb8948f4873ea27fb1f3882bf9dc74bbd32
Ana Vaz <ana.vaz@noaa.edu>
2024-02-29 15:26:50 -0500

    message

--- Codes/EOF_Chl_indexes_wrangling.R
+++ Codes/EOF_Chl_indexes_wrangling.R
@@ -6,7 +6,7 @@ library(R.matlab)
 # now read all EOF Seasonal (PCA for time)
 
 # set the diretory 
-filedir <- "~/Stuff/Current/SAtlantic/data/EOF_CNAPS/ChSeason/"
+filedir <- "~/Stuff/Current/SAtlantic/data/ChlShelf/"
 
 # Get a list of all the files in the directory
 file_list <- list.files(filedir)
@@ -23,9 +23,10 @@ yearsEOF <- seq(1998,2021)
 # create matrix for saving recdevs
 File.out <- matrix(data=NA, nrow=length(yearsEOF), ncol=8, dimnames=list(yearsEOF, NamePlotModes))
 
-png(gsub(" ","",(paste("./images/TimeSeries/EOF_Seasonal_Chl.png"))),res = 300,width = 2000, height = 2000)
+png(gsub(" ","",(paste("../images/TimeSeries/EOF_Seasonal_Chl.png"))),res = 300,width = 2000, height = 2000)
 par(mfrow = c(2, 4), mar=c(.2, .2, .2, .2), oma=c(0, 0, 0, 0), mai=c(.25, .25, .25, .25))
 
+index <- 1
 # Loop through EOF files
 for (inFile in file_list) {
   # Construct the full file path
@@ -41,16 +42,17 @@ for (inFile in file_list) {
     plot(yearsEOF, rep(0,length(yearsEOF)), ylim=c(-limit, limit), type="n", ylab="", xlab="", main = NamePlotModes[index])
     lines(yearsEOF, EOFtime[,i], lty=1, col=4)
     abline(h=0, col="gray70")
+    index <- index + 1
   }
 }
 dev.off()
-write.csv(File.out, file="./csv_files/Chl_EOF_Seasonal.csv", quote=F)
+write.csv(File.out, file="Chl_EOF_Seasonal.csv", quote=F)
 
 ################################################################################
 # now read all EOF Spawning Season (Winter - Feb to April, Summer - Jun to Aug
 
 # set the diretory with the assessment files
-filedir <- "~/Stuff/Current/SAtlantic/data/EOF_CNAPS/ChSpSeason/"
+filedir <- "~/Stuff/Current/SAtlantic/data/ChlShelfSp/"
 
 # Get a list of all the files in the directory
 file_list <- list.files(filedir)
@@ -93,3 +95,50 @@ for (inFile in file_list) {
 dev.off()
 
 write.csv(File.out, file="Chl_EOF_SeasonalSp.csv", quote=F)
+
+
+################################################################################
+# now read all EOF Seasonal (PCA for time)
+
+# set the diretory 
+filedir <- "~/Stuff/Current/SAtlantic/data/ChlModis/"
+
+# Get a list of all the files in the directory
+file_list <- list.files(filedir)
+
+NamePlot <- c("Chl")
+NameSeason <- c("Fall", "Spring", "Summer", "Winter")
+NameModes <- c("Mode 1","Mode 2")
+
+# create combinations of names and combine the names
+aux <- expand.grid(NameModes,NameSeason,NamePlot)
+NamePlotModes <- paste(aux$Var3, aux$Var2, aux$Var1)
+
+yearsEOF <- seq(2003,2022)
+# create matrix for saving recdevs
+File.out <- matrix(data=NA, nrow=length(yearsEOF), ncol=8, dimnames=list(yearsEOF, NamePlotModes))
+
+png(gsub(" ","",(paste("../images/TimeSeries/EOF_Seasonal_Chl_Modis.png"))),res = 300,width = 2000, height = 2000)
+par(mfrow = c(2, 4), mar=c(.2, .2, .2, .2), oma=c(0, 0, 0, 0), mai=c(.25, .25, .25, .25))
+
+index <- 1
+# Loop through EOF files
+for (inFile in file_list) {
+  # Construct the full file path
+  filename <- file.path(filedir, inFile)
+  EOFaux   <- readMat(filename)
+  EOFtime  <- EOFaux$EOFtime
+  # each EOF has 4 modes (saving and plotting only 2 first ones)
+  for (i in 1:2) {
+    # save each mode
+    File.out[,index] = EOFtime[,i]
+    # plot each mode
+    limit <- max(abs(EOFtime[,i]))
+    plot(yearsEOF, rep(0,length(yearsEOF)), ylim=c(-limit, limit), type="n", ylab="", xlab="", main = NamePlotModes[index])
+    lines(yearsEOF, EOFtime[,i], lty=1, col=4)
+    abline(h=0, col="gray70")
+    index <- index + 1
+  }
+}
+dev.off()
+write.csv(File.out, file="Chl_EOF_Seasonal_Modis.csv", quote=F)
--- Codes/correlation_CHL_EOF.R
+++ Codes/correlation_CHL_EOF.R
@@ -12,10 +12,12 @@ library(wesanderson)
 graphics.off()
 # load recdevs, indexes and EOF
 # set the diretory with the assessment files
-#setwd("./csv_files")
+# setwd("./csv_files")
 RecDev  <- read.csv("RecruitmentResiduals_Feb23.csv")
 CHL     <- read.csv("Chl_EOF_SeasonalSp.csv")
-
+CHLSeason <- read.csv("Chl_EOF_Seasonal.csv")
+CHLModis  <- read.csv("Chl_EOF_Seasonal_Modis.csv")
+#
 yearsPlot <- 1993:2021
 color_palette <- rainbow(length(unique(yearsPlot)))
 coloraux = colorRampPalette(brewer.pal(11, "Spectral"))
@@ -32,7 +34,7 @@ NamePlotSeason <- paste(aux$Var2, aux$Var1)
 spNamePlot <- c("Gag Grouper","Greater Amberjack","Gray Triggerfish"," Red Porgy","Red Grouper","Black Sea Bass","Red Snapper","Scamp","Snowy Grouper ","Vermilion Snapper")
 
 #------------------------------------------------------------------
-# For SEASONAL
+# For CHL GlobColour Sp SEASONAL
 graphics.off()
 
 pcut <- 0.1
@@ -111,7 +113,7 @@ for (iSpp in 1:10) {
         theme_minimal()
       
       # Create a grid of the two plots
-      png(gsub(" ","",paste("../images/CorrelationCHLSeason/",spNamePlot[iSpp],NamePlotSeason[iEOF-1],".png")),res = 300, width = 2000, height = 2000)
+      png(gsub(" ","",paste("../images/CorrelationCHLSpSeason/",spNamePlot[iSpp],NamePlotSeason[iEOF-1],".png")),res = 300, width = 2000, height = 2000)
       grid.arrange(plot1, plot2, ncol = 1)
       dev.off()
     }
@@ -124,7 +126,7 @@ plot_matrix[p_matrix > pcut] <- NaN
 colorPlot <- brewer.pal(9, "RdBu")
 
 melted_cor_matrix <- melt(plot_matrix)
-png(gsub(" ","",paste("../images/Correlation_CHL_Season_Species_heatmap.png")),res = 300, width = 2000, height = 2000)
+png(gsub(" ","",paste("../images/Correlation_CHL_SpSeason_Species_heatmap.png")),res = 300, width = 2000, height = 2000)
 ggplot(melted_cor_matrix, aes(x = Var1, y = Var2, fill = value)) +
   geom_tile(color = "white", size = 0.5) +  # Add white grid lines
   scale_fill_gradient2(low = "#3B9AB2", mid = "white", high = "#F21A00", na.value = "white", midpoint = 0) +
@@ -144,7 +146,7 @@ plot_matrix[p_matrix > 0.05] <- NaN
 colorPlot <- brewer.pal(9, "RdBu")
 
 melted_cor_matrix <- melt(plot_matrix)
-png(gsub(" ","",paste("../images/Correlation_CHL_Season_Species_heatmap05.png")),res = 300, width = 2000, height = 2000)
+png(gsub(" ","",paste("../images/Correlation_CHL_SpSeason_Species_heatmap05.png")),res = 300, width = 2000, height = 2000)
 ggplot(melted_cor_matrix, aes(x = Var1, y = Var2, fill = value)) +
   geom_tile(color = "white", size = 0.5) +  # Add white grid lines
   scale_fill_gradient2(low = "#3B9AB2", mid = "white", high = "#F21A00", na.value = "white", midpoint = 0) +
@@ -159,33 +161,41 @@ ggplot(melted_cor_matrix, aes(x = Var1, y = Var2, fill = value)) +
 dev.off()
 
 #------------------------------------------------------------------
-# For Spawning SEASONAL
+# For GlobColour SEASONAL
 graphics.off()
 
-# create a matrix with only dates both have data
-EOFSpSeason_RecDev <- merge(EOFSpSeason, RecDev, by = "X")
+# it is in alphabetical order
+NamePlot <- c("Mode 1","Mode 2")
+NameSeason <- c("Fall","Spring","Summer","Winter")
 
-# we have 5 Anomal variables, vs 2 seasons (10) and 10 species
-itot <- (5*2)+1
+# create combinations of names and combine the names
+aux <- expand.grid(NamePlot, NameSeason)
+NamePlotSeason <- paste(aux$Var2, aux$Var1)
+
+# create a matrix with only dates both have data
+EOFSeason_RecDev <- merge(CHLSeason, RecDev, by = "X")
+
+# we have 1 variables, vs 4 seasons (4) vs 2 modes (8) and 10 species
+itot <- (4*2)+1
 
 # for results of EOF vs indexes
-corr_matrix <- matrix(NA, nrow=itot-1, ncol=10, dimnames=list(NamePlotModesSpawnSeason, spNamePlot))
-p_matrix    <- matrix(NA, nrow=itot-1, ncol=10, dimnames=list(NamePlotModesSpawnSeason, spNamePlot))
+corr_matrix <- matrix(NA, nrow=itot-1, ncol=10, dimnames=list(NamePlotSeason, spNamePlot))
+p_matrix    <- matrix(NA, nrow=itot-1, ncol=10, dimnames=list(NamePlotSeason, spNamePlot))
 
-yearsPlot <- unique(EOFSpSeason_RecDev[,1])
+yearsPlot <- unique(EOFSeason_RecDev[,1])
 
 for (iSpp in 1:10) {
   for (iEOF in 2:itot) {
     # correlation and save coef. and p
-    x <- EOFSpSeason_RecDev[,iEOF]
-    y <- EOFSpSeason_RecDev[,iSpp+itot]
+    x <- EOFSeason_RecDev[,iEOF]
+    y <- EOFSeason_RecDev[,iSpp+itot]
     aux <- cor.test(x, y, use = "complete.obs")
     corr_matrix[iEOF-1,iSpp] <- aux$estimate
     p_matrix[iEOF-1,iSpp] <- aux$p.value
     
     # correlation and save coef. and p
-    x <- EOFSpSeason_RecDev[,iEOF]
-    y <- EOFSpSeason_RecDev[,iSpp+itot]
+    x <- EOFSeason_RecDev[,iEOF]
+    y <- EOFSeason_RecDev[,iSpp+itot]
     aux <- cor.test(x, y, use = "complete.obs")
     corr_matrix[iEOF-1,iSpp] <- aux$estimate
     p_matrix[iEOF-1,iSpp] <- aux$p.value
@@ -197,7 +207,7 @@ for (iSpp in 1:10) {
       y = y,
       estimate = aux$estimate,
       p.value = aux$p.value,
-      color = as.numeric(factor(EOFSpSeason_RecDev[,1]))
+      color = as.numeric(factor(EOFSeason_RecDev[,1]))
     )
     df <- df[complete.cases(df), ]
     
@@ -212,7 +222,7 @@ for (iSpp in 1:10) {
                 x = Inf, y = Inf, hjust = 1, vjust = 0, size = 5) +
       scale_color_manual(values = c("Var 1" = "#F21A00", "Var 2" = "#3B9AB2")) +
       labs(
-        title = paste(NamePlotModesSpawnSeason[iEOF-1], spNamePlot[iSpp]),
+        title = paste(NamePlotSeason[iEOF-1], spNamePlot[iSpp]),
         y = NULL,
         x = NULL
       ) +
@@ -240,7 +250,7 @@ for (iSpp in 1:10) {
       theme_minimal()
     
     # Create a grid of the two plots
-    png(gsub(" ","",paste("../images/CorrelationAnomalSeasonSp/",spNamePlot[iSpp],NamePlotModesSpawnSeason[iEOF-1],".png")),res = 300, width = 2000, height = 2000)
+    png(gsub(" ","",paste("../images/CorrelationCHLSeason/",spNamePlot[iSpp],NamePlotSeason[iEOF-1],".png")),res = 300, width = 2000, height = 2000)
     grid.arrange(plot1, plot2, ncol = 1)
     dev.off()
   } # end iEOF, lwd 
@@ -252,7 +262,7 @@ plot_matrix[p_matrix > pcut] <- NaN
 colorPlot <- brewer.pal(9, "RdBu")
 
 melted_cor_matrix <- melt(plot_matrix)
-png(gsub(" ","",paste("../images/Correlation_Anomal_SpSeason_Species_heatmap.png")),res = 300, width = 2000, height = 2000)
+png(gsub(" ","",paste("../images/Correlation_CHL_Season_Species_heatmap.png")),res = 300, width = 2000, height = 2000)
 ggplot(melted_cor_matrix, aes(x = Var1, y = Var2, fill = value)) +
   geom_tile(color = "white", size = 0.5) +  # Add white grid lines
   scale_fill_gradient2(low = "#3B9AB2", mid = "white", high = "red", na.value = "white", midpoint = 0) +
@@ -265,3 +275,122 @@ ggplot(melted_cor_matrix, aes(x = Var1, y = Var2, fill = value)) +
     panel.grid.minor = element_line(color = "lightgray", size = 0.5)  # Remove minor grid lines
   )
 dev.off()
+
+
+#------------------------------------------------------------------
+# For Modis SEASONAL
+graphics.off()
+
+# it is in alphabetical order
+NamePlot <- c("Mode 1","Mode 2")
+NameSeason <- c("Fall","Spring","Summer","Winter")
+
+# create combinations of names and combine the names
+aux <- expand.grid(NamePlot, NameSeason)
+NamePlotSeason <- paste(aux$Var2, aux$Var1)
+
+# create a matrix with only dates both have data
+EOFSeason_RecDev <- merge(CHLModis, RecDev, by = "X")
+
+# we have 1 variables, vs 4 seasons (4) vs 2 modes (8) and 10 species
+itot <- (4*2)+1
+
+# for results of EOF vs indexes
+corr_matrix <- matrix(NA, nrow=itot-1, ncol=10, dimnames=list(NamePlotSeason, spNamePlot))
+p_matrix    <- matrix(NA, nrow=itot-1, ncol=10, dimnames=list(NamePlotSeason, spNamePlot))
+
+yearsPlot <- unique(EOFSeason_RecDev[,1])
+
+for (iSpp in 1:10) {
+  for (iEOF in 2:itot) {
+    # correlation and save coef. and p
+    x <- EOFSeason_RecDev[,iEOF]
+    y <- EOFSeason_RecDev[,iSpp+itot]
+    aux <- cor.test(x, y, use = "complete.obs")
+    corr_matrix[iEOF-1,iSpp] <- aux$estimate
+    p_matrix[iEOF-1,iSpp] <- aux$p.value
+    
+    # correlation and save coef. and p
+    x <- EOFSeason_RecDev[,iEOF]
+    y <- EOFSeason_RecDev[,iSpp+itot]
+    aux <- cor.test(x, y, use = "complete.obs")
+    corr_matrix[iEOF-1,iSpp] <- aux$estimate
+    p_matrix[iEOF-1,iSpp] <- aux$p.value
+    # TEST WITH GGPLOT
+    # Create a data frame with your data
+    df <- data.frame(
+      yearsPlot = yearsPlot,
+      x = x,
+      y = y,
+      estimate = aux$estimate,
+      p.value = aux$p.value,
+      color = as.numeric(factor(EOFSeason_RecDev[,1]))
+    )
+    df <- df[complete.cases(df), ]
+    
+    # Create the first panel with different axes
+    plot1 <- ggplot(df, aes(x = yearsPlot)) +
+      geom_line(aes(y = x, color = "Var 1"), linetype = "solid", linewidth = 1.5) +
+      geom_line(aes(y = y, color = "Var 2"), linetype = "solid", linewidth = 1.5) +
+      geom_hline(yintercept = 0, color = "gray70", linetype = "dashed", linewidth = 1) +
+      geom_text(aes(label = paste("R=", format(estimate, digits = 2))), 
+                x = Inf, y = Inf, hjust = 1, vjust = 0, size = 5) +
+      geom_text(aes(label = paste("p=", format(p.value, digits = 2))), 
+                x = Inf, y = Inf, hjust = 1, vjust = 0, size = 5) +
+      scale_color_manual(values = c("Var 1" = "#F21A00", "Var 2" = "#3B9AB2")) +
+      labs(
+        title = paste(NamePlotSeason[iEOF-1], spNamePlot[iSpp]),
+        y = NULL,
+        x = NULL
+      ) +
+      theme_minimal() +
+      theme(
+        legend.position = "top",
+        legend.title = element_blank()
+      )
+    pal <- wes_palette("Zissou1", 30, type = "continuous")
+    # Create the second panel with a color gradient
+    plot2 <- ggplot(df, aes(x = x, y = y)) +
+      geom_point(aes(colour = yearsPlot),  size = 3) +
+      geom_smooth(method = "lm", formula = y ~ x, color = "black", linetype = "dashed", se = FALSE) +
+      geom_ribbon(
+        aes(ymin = predict(lm(y ~ x), newdata = df, interval = "confidence")[, "lwr"],
+            ymax = predict(lm(y ~ x), newdata = df, interval = "confidence")[, "upr"]),
+        fill = "gray80", alpha = 0.5  # Adjust the alpha value here
+      ) +
+      scale_color_gradientn(colors = pal, name = "") +
+      labs(
+        x = "PCAs",
+        y = "Residuals",
+        title = paste("R=",format(aux$estimate, digits = 2)," p=",format(aux$p.value , digits = 2))
+      ) +
+      theme_minimal()
+    
+    # Create a grid of the two plots
+    png(gsub(" ","",paste("../images/CorrelationCHLModis/",spNamePlot[iSpp],NamePlotSeason[iEOF-1],".png")),res = 300, width = 2000, height = 2000)
+    grid.arrange(plot1, plot2, ncol = 1)
+    dev.off()
+  } # end iEOF, lwd 
+}  # end iInd
+
+plot_matrix <- corr_matrix
+plot_matrix[p_matrix > pcut] <- NaN
+# plot the correlation
+colorPlot <- brewer.pal(9, "RdBu")
+
+melted_cor_matrix <- melt(plot_matrix)
+png(gsub(" ","",paste("../images/Correlation_CHL_Modis_Season_Species_heatmap.png")),res = 300, width = 2000, height = 2000)
+ggplot(melted_cor_matrix, aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile(color = "white", size = 0.5) +  # Add white grid lines
+  scale_fill_gradient2(low = "#3B9AB2", mid = "white", high = "red", na.value = "white", midpoint = 0) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 90, hjust = 1),
+    panel.background = element_rect(fill = "white"),  # Set the background color to white
+    plot.background = element_rect(fill = "white"), # Set the plot background to white
+    panel.grid.major = element_line(color = "lightgray", size = 0.5),  # Add light gray grid lines
+    panel.grid.minor = element_line(color = "lightgray", size = 0.5)  # Remove minor grid lines
+  )
+dev.off()
+
+
